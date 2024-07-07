57b092565ceb44a2ec9599e880bab402a813628a
tilak-atri <tilak@electorq.com>
2024-07-07 16:16:38 +0530

    Upload folder added

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -5,17 +5,11 @@ import ProcessingRequest from '../models/processingRequest.js';
 import { processImages } from '../utils/imageProcessingService.js';
 
 export const handleFileUpload = async (req, res) => {
-  const { file } = req;
+  const filePath = req.file.path;
   const requestId = uuidv4();
 
-  if (!file) {
-    return res.status(400).send('No file uploaded');
-  }
-
   try {
-    const filePath = file.path; 
     const records = await validateCsv(filePath);
-
     for (const record of records) {
       const serialNumber = record['S. No.'];
       const productName = record['Product Name'];
@@ -31,14 +25,11 @@ export const handleFileUpload = async (req, res) => {
       await processingRequest.save();
       processImages(requestId, serialNumber, productName, imageUrls); 
     }
-
     res.status(200).json({ requestId });
   } catch (error) {
     console.error('Error processing CSV file:', error);
     res.status(400).send(`Error processing CSV file: ${error}`);
   } finally {
-    if (filePath) {
-      fs.remove(filePath); 
-    }
+    fs.remove(filePath);
   }
 };
